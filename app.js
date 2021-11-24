0c217f3fdc6908dcf395fea5b736976a695df077
Daniel <68166857+daholz@users.noreply.github.com>
2021-11-24 13:25:29 -0600

    Added battle log capability

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const app = Vue.createApp({
             playerHealth: 100,
             monsterHealth: 100,
             currentRound: 0,
-            winner: null
+            winner: null,
+            battleLog: []
         };
     },
     watch: {
@@ -43,25 +44,25 @@ const app = Vue.createApp({
         specialAttackTimer() {
             return this.currentRound % 3 !== 0;
         },
-        gameOver() {
-            return this.winner !== null;
-        }
     },
     methods: {
         attackMonster() {
             this.currentRound++;
             const attackValue = getRandomValue(5, 12);
             this.monsterHealth -= attackValue;
+            this.addLogMessage('player', 'attack', attackValue);
             this.attackPlayer();
         },
         attackPlayer() {
             const attackValue = getRandomValue(8, 15);
             this.playerHealth -= attackValue;
+            this.addLogMessage('monster', 'attack', attackValue);
         },
         specialAttack() {
             this.currentRound++;
             const attackValue = getRandomValue(10,25);
             this.monsterHealth -= attackValue;
+            this.addLogMessage('player', 'attack', attackValue);
             this.attackPlayer();
         },
         healPlayer() {
@@ -72,6 +73,7 @@ const app = Vue.createApp({
             } else {
             this.playerHealth += healValue;
             }
+            this.addLogMessage('player', 'heal', healValue);
             this.attackPlayer();
         },
         startNewGame() {
@@ -79,9 +81,17 @@ const app = Vue.createApp({
             this.monsterHealth = 100;
             this.currentRound = 0;
             this.winner = null;
+            this.battleLog = [];
         },
         surrender() {
             this.winner = 'monster';
+        },
+        addLogMessage(who, what, value) {
+            this.battleLog.unshift({
+                actionBy: who,
+                actionType: what,
+                actionValue: value
+            });
         }
     }
 });
